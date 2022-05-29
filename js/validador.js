b4103b9418c1a6c5a6f8b6ace29717b65dd56472
wasdavi <wasdavi@gmail.com>
2022-05-29 17:37:53 -0300

    s

---
+++ js/validador.js
@@ -0,0 +1,16 @@
+
+const btn = document.querySelector('#entrar')
+
+
+
+/*function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login=="adm" && senha=="adm"){
+        window.location.replace("https://www.google.com.br/");
+    }else{
+        window.location.replace("https://www.google.com.br/");
+    }
+}*/
+
