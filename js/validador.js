ca6429a2a11fe0648e1fef5881e166abd8479226
wasdavi <wasdavi@gmail.com>
2022-05-29 17:59:35 -0300

    xca

--- js/validador.js
+++ js/validador.js
@@ -1,16 +1,12 @@
 
-const btn = document.querySelector('#entrar')
-
-
-
-/*function logar(){
+function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
     if(login=="adm" && senha=="adm"){
         window.location.replace("https://www.google.com.br/");
     }else{
-        window.location.replace("https://www.google.com.br/");
+        alert("Login ou senha incorretos");
     }
-}*/
+}
 
